b946649db91f97c59c26fb8daef2100cf959367f
AncientBlaze <saikata62@gmail.com>
2025-02-27 16:50:36 +0530

    updated frontend

--- frontend/src/Components/ErrorsPages/404Page.jsx
+++ frontend/src/Components/ErrorsPages/404Page.jsx
@@ -1,17 +1,12 @@
+// import Lottie from "lottie-react";
+// import animationData from './path/to/your/lottie-file.json';
 
-import { Link } from 'react-router';
+function ErrorsPage() {
+  return (
+    <div>
+      {/* <Lottie animationData={animationData} loop={true} /> */}
+    </div>
+  );
+}
 
-const NotFoundPage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-            <h1 className="text-6xl font-bold mb-4">404</h1>
-            <p className="text-xl mb-8">Sorry, the page you are looking for does not exist.</p>
-            <Link to="/" className="text-blue-500 hover:underline">
-                Go to Homepage
-            </Link>
-        </div>
-    );
-};
-
-export default NotFoundPage;
-
+export default ErrorsPage;
